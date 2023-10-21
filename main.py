0b26e45a5fe0c6e48ee732d4e3043fa45d27f541
Odessa <ukrainskiykozak23@gmail.com>
2023-10-21 17:56:40 +0300

    live of student 3vers

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
+import random
 class Student:
-    group = "C2016"
-    counter = 0
-    def __init__(self, age, height, name=None):
-        self.name = name
-        self.age = age
-        self.height = height
-        Student.counter += 1
-        print(id("Объект был создан "))
-    def grow(self):
-        print("shfjdlkseajfklasjd")
-        self.height += 10
-    def __str__(self):
-        return f"I'm {self.name}. My age is {self.age}"
-    def __del__(self):
-        print("Удален объект")
 
-print(Student.group)
-nick = Student(16,  165, "Nick")
-print(nick)
-# print(nick.age)
-# print(nick.height)
-# print(nick.name)
-# print(nick.group)
-# kate = Student(17, 175, "Ekatirina")
-# print(kate.height)
-# print(kate.age)
-# print(kate.name)
-# print(kate.group)
-# elena = Student(18, 160, "Elena")
-# print(Student.counter)
-# print("Nick height", nick.height)
-# nick.grow()
-# print("Nick height", nick.height)
+    def __init__(self, name=None):
+        self.name = name
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
+
+    def to_study(self):
+        print("Time to study")
+        self.progress += 0.15
+        self.gladness -= 5
+    def to_sleep(self):
+        print("I will sleep")
+        self.gladness += 3
+    def to_chill(self):
+        print("Rest time")
+        self.gladness += 3
+        self.progress -= 0.1
+    def is_alive(self):
+        if self.progress < -0.5:
+            print("Can't out...")
+            self.alive = False
+        elif self.gladness <= 0:
+            print("Depression")
+            self.alive = False
+        elif self.progress > 5:
+            print("Passed externally")
+            self.alive = False
+
+    def end_of_day(self):
+        print(f"Glandess = {self.gladness}")
+        print(f"Progress = {self.progress}")
+    def live(self, day):
+        s = f"Day {day} of {self.name} live"
+        print(f"{s:=^50}")
+        live_cube = random.randint(1,3)
+        if live_cube == 1:
+            self.to_chill()
+        elif live_cube == 3:
+            self.to_study()
+        self.end_of_day()
+        self.is_alive()
+nick = Student(name="Nikolas")
+
+for day in range(365):
+    if nick.alive == False:
+        break
+    nick.live(day)
